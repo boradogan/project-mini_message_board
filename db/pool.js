8dc31d6778949d8153deb8f5fae3474590eeeea2
boradogan <doan.bora@gmail.com>
2025-09-14 17:44:14 +0300

    changed the cert location

--- db/pool.js
+++ db/pool.js
@@ -9,7 +9,8 @@ const __dirname = path.dirname(__filename);
 export default new Pool({
   connectionString: process.env.DATABASE_URL,
   ssl: {
-    rejectUnauthorized: true,
-    ca: fs.readFileSync(path.join(__dirname, '..', 'certificates', 'prod-ca-2021.crt')),
+    rejectUnauthorized: false,
+    // ca: fs.readFileSync(path.join(__dirname, '..', 'certificates', 'prod-ca-2021.crt')),
+    ca:process.env.DATABASE_CA_CERT
   }
 });
