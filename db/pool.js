170b08640fb50337a054095c7bacc85504e7db78
boradogan <doan.bora@gmail.com>
2025-09-14 17:11:51 +0300

    added SSL cerrificate of the database

--- db/pool.js
+++ db/pool.js
@@ -1,5 +1,15 @@
+import fs from 'fs';
+import path from 'path';
 import { Pool } from "pg";
 import 'dotenv/config'
+import { fileURLToPath } from 'url';
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
 export default new Pool({
   connectionString: process.env.DATABASE_URL,
+  ssl: {
+    rejectUnauthorized: true,
+    ca: fs.readFileSync(path.join(__dirname, '..', 'certificates', 'prod-ca-2021.crt')),
+  }
 });
